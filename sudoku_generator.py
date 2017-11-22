b6046ededdbfa43eb114c0c21f056465aef9cb3d
fiorezhang <fiorezhang@sina.com>
2017-11-22 10:28:17 +0800

    Add files via upload

---
+++ sudoku_generator.py
@@ -0,0 +1,147 @@
+import numpy as np
+
+
+#define the size of matrix
+SIZE = 3
+
+m_basic = np.zeros((SIZE * SIZE, SIZE * SIZE), dtype='int8')
+
+#calculate the remainder for x in range: 1~y
+def remainder(x, y):
+	return (x-1)%y+1
+
+'''too complicated, easier way as below
+#generate the 1st piece with SIZE * SIZE size
+temp = 0
+for i in range(SIZE):
+	for j in range(SIZE):
+		temp += 1
+		m_basic[i, j] = temp
+		
+#generate the 1st line of pieces with (SIZE*SIZE) * SIZE size
+for i in range(SIZE):
+	for k in range(SIZE):
+		for j in range(SIZE):
+			m_basic[i, k*SIZE+j] = remainder((m_basic[i, j] + k*SIZE), SIZE*SIZE)
+		
+#generate the whole matrix
+for k in range(SIZE):
+	for i in range(SIZE):
+		for j in range(SIZE*SIZE):
+			m_basic[k*SIZE+i, j] = remainder((m_basic[i, j] + k), SIZE*SIZE)
+'''
+
+#generate one matrix which fulfill the sudoku rule, as the basic for others
+for i in range(SIZE*SIZE):
+	for j in range(SIZE*SIZE):
+		m_basic[i, j] = remainder((1 + SIZE*(i%SIZE) + int(i/SIZE) + j), SIZE*SIZE)
+#print(m_basic)
+
+'''define some functions to generate new matrix based from the original matrix, 
+	including swap rows within same bundle, or swap two bundles of rows, 
+	similarlly to the columns, finally we make a re-map from 1~n to a random sort of 1~n
+'''
+#swap rows within a bundle, check the row number
+def swap_row(m, x, y):
+	s = m.shape[0]#columns/rows in matrix
+	b = (int)(np.sqrt(s))#bundles of columns/rows in matrix
+	t = np.zeros(s)#use to buffer the data to be swap
+	#check whether x and y in a bundle
+	if (int(x/b) != int(y/b)) or (x < 0) or (x >= s) or (y < 0) or (y >= s) or (x == y):
+		print('ERROR'),
+		return m
+	
+	for j in range(s):
+		t[j] = m[x, j]
+		m[x, j] = m[y, j]
+		m[y, j] = t[j]
+		
+	return m
+
+#swap bundles of rows, check the bundle number
+def swap_row_b(m, x, y):
+	s = m.shape[0]#columns/rows in matrix
+	b = (int)(np.sqrt(s))#bundles of columns/rows in matrix
+	t = np.zeros(s)#use to buffer the data to be swap
+	#check x and y
+	if (x < 0) or (x >= b) or (y < 0) or (y >= b) or (x == y): 
+		print('ERROR'),
+		return m
+		
+	for k in range(b):
+		for j in range(s):
+			t[j] = m[x*b+k, j]
+			m[x*b+k, j] = m[y*b+k, j]
+			m[y*b+k, j] = t[j]
+			
+	return m
+
+#swap columns within a bundle, check the colunm number
+def swap_col(m, x, y):
+	s = m.shape[0]#columns/rows in matrix
+	b = (int)(np.sqrt(s))#bundles of columns/rows in matrix
+	t = np.zeros(s)#use to buffer the data to be swap
+	#check whether x and y in a bundle
+	if (int(x/b) != int(y/b)) or (x < 0) or (x >= s) or (y < 0) or (y >= s) or (x == y):
+		print('ERROR'),
+		return m
+	
+	for i in range(s):
+		t[i] = m[i, x]
+		m[i, x] = m[i, y]
+		m[i, y] = t[i]
+		
+	return m
+
+#swap bundles of columns, check the bundle number
+def swap_col_b(m, x, y):
+	s = m.shape[0]#columns/rows in matrix
+	b = (int)(np.sqrt(s))#bundles of columns/rows in matrix
+	t = np.zeros(s)#use to buffer the data to be swap
+	#check x and y
+	if (x < 0) or (x >= b) or (y < 0) or (y >= b) or (x == y): 
+		print('ERROR'),
+		return m
+		
+	for k in range(b):
+		for i in range(s):
+			t[i] = m[i, x*b+k]
+			m[i, x*b+k] = m[i, y*b+k]
+			m[i, y*b+k] = t[i]
+			
+	return m
+
+#remap 1~n in matrix into a random shuffled serial of 1~n, ex: [123456789]->[597326841]
+def remap(m):
+	s = m.shape[0]
+	n = np.zeros((s, s), dtype='int8')#new a matrix to contain the remapped data
+	t = np.zeros(s, dtype='int8')
+	
+	for i in range(s):
+		t[i] = i+1
+	np.random.shuffle(t)
+	print(t)
+	
+	for i in range(s):
+		for j in range(s):
+			n[i, j] = t[(m[i, j]-1)]
+			
+	return n
+	
+def shuffle(m):
+	s = m.shape[0]
+	b = (int)(np.sqrt(s))
+	
+	return m
+
+
+######
+print(m_basic)
+print(remap(m_basic))	
+'''
+print(m_basic),
+print(swap_row(m_basic, 6, 8)),
+print(swap_row_b(m_basic, 0, 2)),
+print(swap_col(m_basic, 0, 1)),
+print(swap_col_b(m_basic, 1, 2)),
+'''
